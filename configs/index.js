fc046bfafe79e39be38a96bf4426e1f6284e6560
Nawar Hisso <nawar@polytrade.finance>
2022-10-06 14:37:43 +0400

    :building_construction: Add Configs File

---
+++ configs/index.js
@@ -0,0 +1,7 @@
+const CONTRACT_NAMES = ['NFT'];
+const NFT_NAME = 'Polytrade';
+const NFT_SYMBOL = 'TRADE';
+
+const CONFIGS = { CONTRACT_NAMES, NFT_NAME, NFT_SYMBOL };
+
+exports.default = CONFIGS;
