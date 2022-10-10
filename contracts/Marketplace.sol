be7658a1538d350544bfcb46251f5aaf735776c1
Nawar Hisso <nawar@polytrade.finance>
2022-10-10 10:52:15 +0400

    :rocket: Create Marketplace Smart Contract

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+
+/**
+ * @title The common marketplace for the AssetNFTs
+ * @author Polytrade.Finance
+ * @dev Implementation of all AssetNFT trading operations
+ */
+contract Marketplace {
+
+}
