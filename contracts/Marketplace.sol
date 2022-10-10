8588d6b55b9ecaa5f049742329a270e038d94e39
Nawar Hisso <nawar@polytrade.finance>
2022-10-10 15:16:30 +0400

    :sparkles: Make The Marketplace As A Receiver

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,11 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.14;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
 /**
  * @title The common marketplace for the AssetNFTs
  * @author Polytrade.Finance
  * @dev Implementation of all AssetNFT trading operations
  */
-contract Marketplace {
-
+contract Marketplace is IERC721Receiver {
+    /**
+     * @dev Whenever an {IERC721} `tokenId` token is transferred to this contract via {IERC721-safeTransferFrom}
+     * by `operator` from `from`, this function is called.
+     *
+     * It must return its Solidity selector to confirm the token transfer.
+     * If any other value is returned or the interface is not implemented by the recipient,
+     * the transfer will be reverted.
+     *
+     * The selector can be obtained in Solidity with `IERC721Receiver.onERC721Received.selector`.
+     */
+    function onERC721Received(
+        address,
+        address,
+        uint,
+        bytes calldata
+    ) external pure returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
 }
