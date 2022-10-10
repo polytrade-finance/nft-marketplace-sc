15bd8675f2c1127e8d558deac34688d2260db475
Nawar Hisso <nawar@polytrade.finance>
2022-10-10 15:16:48 +0400

    :white_check_mark: Add Test Cases

--- test/Marketplace.js
+++ test/Marketplace.js
@@ -5,6 +5,9 @@ const hre = require('hardhat');
 const { default: CONTRACTS } = require('../configs/contracts.js');
 
 describe('Marketplace', function () {
+  const _metadata = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const _tokenId = 0;
+
   async function deploy() {
     const Marketplace = await hre.ethers.getContractFactory(CONTRACTS.NAMES[1]);
     const marketplace = await Marketplace.deploy();
@@ -12,9 +15,27 @@ describe('Marketplace', function () {
     return { marketplace };
   }
 
+  async function deployToken() {
+    const [owner, otherAddress] = await hre.ethers.getSigners();
+
+    const NFT = await hre.ethers.getContractFactory(CONTRACTS.NAMES[0]);
+    const nft = await NFT.deploy(CONTRACTS.NFT_NAME, CONTRACTS.NFT_SYMBOL);
+
+    return { nft, owner, otherAddress };
+  }
+
   describe('Deployment', function () {
-    it('Marketplace deployment test', async function () {
+    it('Marketplace should be able to receive AssetNFTs', async function () {
       const { marketplace } = await loadFixture(deploy);
+      const { nft, owner } = await loadFixture(deployToken);
+
+      await nft.createAsset(owner.address, _tokenId, _metadata);
+
+      expect(await nft.ownerOf(_tokenId)).to.equal(owner.address);
+
+      await nft.transferFrom(owner.address, marketplace.address, _tokenId);
+
+      expect(await nft.ownerOf(_tokenId)).to.equal(marketplace.address);
     });
   });
 });
