8dfb4ffe0d0771e1c8d4b0b14fee1f254e2344e0
Nawar Hisso <nawar@polytrade.finance>
2022-10-24 18:04:18 +0400

    :art: Update Marketplace Import Statements

--- contracts/Marketplace/Marketplace.sol
+++ contracts/Marketplace/Marketplace.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "../AssetNFT/IAssetNFT.sol";
 
